37510875559123d1b31630d224d6a0fc6c1bf9c1
zhaozhongyang <1031839775@qq.com>
2017-07-31 18:04:35 +0800

    zzy

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
+
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
     } else {
       //调用登录接口
       wx.getUserInfo({
