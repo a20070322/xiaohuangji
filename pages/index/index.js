17b1223edbf9c6b0cffeae56b3b910b99e5ae181
zhaozhongyang <1031839775@qq.com>
2017-08-02 17:31:56 +0800

    xiaohuangji

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    mytext:"",
     mydata:[
       {
         type:"user",
@@ -16,17 +17,43 @@ Page({
       }
     ]
   },
-  mysend:()=>{
+  bindinput:function(e){
+    this.setData({
+      mytext: e.detail.value
+    })
+  },
+  mysend:function(){
+    this.data.mydata.push({type:"user",text:this.data.mytext})
+    // console.log(tmp)
+    let text = this.data.mytext
+    let that =this
+    this.setData({
+      mydata: this.data.mydata,
+      mytext:""
+    })
     wx.request({
-      url: 'https://www.ahh5.com/api',
-      method: 'get',
+      url: 'https://www.ahh5.com/i',
+      method: 'GET',
+      header: {
+         'content-type': 'application/json'
+     },
       data: {
         key: "free",
         appid: "0",
-        msg: encodeURI("你好")
+        msg: encodeURI(text)
       },
       success: function (res) {
-        console.log(res.data)
+        let rex=/{br}/g
+        let text = res.data.content
+        console.log(text)
+        // if(text.test(rex)){
+        //   // text = text.replace(rex,"\n")
+        // }
+        // tmp = tmp.replace(/{face:.*}/g,"")
+        that.data.mydata.push({type:"feifei",text:text})
+        that.setData({
+          mydata: that.data.mydata
+        })
       },
       fail: function (res) { },
       complete: function (res) { },
@@ -36,55 +63,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
